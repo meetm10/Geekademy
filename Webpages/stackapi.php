222db7a92aeaf275901a50abaaa96b4b6a1243c7
thejaelal <jay.lal@somaiya.edu>
2018-03-02 18:10:19 +0530

    added bug3

--- Webpages/stackapi.php
+++ Webpages/stackapi.php
@@ -1,17 +1,17 @@
 <?php 
-/*
-				$response = file_get_contents("http://api.stackexchange.com/2.1/questions/34417209?site=stackoverflow");
-				$file = 'ads.json';
-				file_put_contents($file, $response);
 
-				$str = file_get_contents('ads.json');
-				$json = json_decode($str, true);
-				echo $json
-				
-				/*echo json_decode($response);
-				$res = json_decode($response);
-				echo gettype($res);
-*/				
-				echo "Hello World"
+	#$response = file_get_contents("http://api.stackexchange.com/2.1/questions/34417209?site=stackoverflow");
+	$file = 'ads.json';
+	file_put_contents($file, "chutya");
+
+	// $str = file_get_contents('ads.json');
+	// $json = json_decode($str, true);
+	// echo $json
+	
+	// echo json_decode($response);
+	// $res = json_decode($response);
+	// echo gettype($res);
+		
+	echo "Hello World";
 
 ?>
