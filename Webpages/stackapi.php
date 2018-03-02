fb027c64715ccc02c0f3d2b17cfa90897afa66ea
thejaelal <jay.lal@somaiya.edu>
2018-03-02 18:00:21 +0530

    test

--- Webpages/stackapi.php
+++ Webpages/stackapi.php
@@ -1,4 +1,5 @@
 <?php 
+/*
 				$response = file_get_contents("http://api.stackexchange.com/2.1/questions/34417209?site=stackoverflow");
 				$file = 'ads.json';
 				file_put_contents($file, $response);
@@ -6,8 +7,10 @@
 				$str = file_get_contents('ads.json');
 				$json = json_decode($str, true);
 				echo $json
+				
 				/*echo json_decode($response);
 				$res = json_decode($response);
 				echo gettype($res);
 				*/
+				echo "Hello World";
 ?>
